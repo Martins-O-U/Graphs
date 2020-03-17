605a06597c1d60b8dd25449f4c93b3f2ee669ac3
HP <martinsonyedikachi@gmail.com>
2020-03-17 06:23:38 +0100

    Added Vertices, Edge, BFS

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,33 +13,45 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertices_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else raise IndexError("That vertices Does not exist")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        q = Queue()
+        q.enqueue(starting_vertex)
+        visited = set()
+
+        while q.size > 0:
+            v = q.dequeue()
+            if v is not in visited:
+                visted.add(v)
+                print(v)
+                for j in self.vertices[v]:
+                    q.enqueue(j)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+
 
     def dft_recursive(self, starting_vertex):
         """
